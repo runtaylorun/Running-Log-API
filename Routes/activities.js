417721478866af3186fe2908c34232fe994741e8
Taylor Mills <taylormills190@gmail.com>
2020-08-06 20:36:50 -0500

    Updated routes

--- Routes/activities.js
+++ Routes/activities.js
@@ -3,9 +3,11 @@ const database = require('../DB/database');
 const connection = database.getConnection();
 
 module.exports = (app) => {
-	app.get('/activity/:id', async (req, res) => {
-		const activity = await connection.query('SELECT * FROM Activities', (err, results, fields) => {
-			res.send(results[0]);
-		});
-	});
+	app.post('/activity', async (req, res) => {});
+
+	app.get('/activity/:id', async (req, res) => {});
+
+	app.put('/activity/:id', async (req, res) => {});
+
+	app.delete('/activity/:id', async (req, res) => {});
 };
