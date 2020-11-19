4d9ae4a22d88d5e7a0623d6c52e0d8d732da45f6
Taylor Mills <trmills@eagles.usi.edu>
2020-11-19 08:51:20 -0500

    Updat db config to use values from app config

--- DB/database.js
+++ DB/database.js
@@ -1,15 +1,16 @@
 const mysql = require('mysql');
+const config = require('../config.json')
 
 let connection;
 
 const connectToDatabase = async () => {
 	try {
 		const init = await mysql.createConnection({
-			host: 'localhost',
-			port: 3306,
-			user: 'Taylor',
-			password: 'Warsawcc@12',
-			database: 'sys',
+			host: config.DBHost,
+			port: config.DBPort,
+			user: config.DBUser,
+			password: config.DBPassword,
+			database: config.DBName,
 			insecureAuth: true,
 		});
 		connection = init;
